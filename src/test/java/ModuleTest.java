5ca57db062c341161e59b14814f3a4c54f1d3c92
romanfomin <rofominucvt@mail.ru>
2018-04-12 23:16:28 +0300

    csv

--- src/test/java/ModuleTest.java
+++ src/test/java/ModuleTest.java
@@ -8,14 +8,32 @@ public class ModuleTest {
 
     Computable module;
     double from, to ,step, accuracy;
+    Sin sin = new Sin();
+    Cos cos = new Cos(sin);
+
+    Ln ln = new Ln();
+
+    Csc csc = new Csc(sin);
+    Cot cot = new Cot(sin, cos);
+    Tan tan = new Tan(cos, sin);
+    Sec sec = new Sec(cos);
+
+    Log3 log3 = new Log3(ln);
+    Log5 log5 = new Log5(ln);
+    Log10 log10 = new Log10(ln);
+
+    LogarithmicFunction logarithmicFunction = new LogarithmicFunction(ln, log3, log5, log10);
+    TrigonometricFunction trigonometricFunction = new TrigonometricFunction(csc, cot, cos, tan, sec);
+
 
     @Test
     public void testMethod1(){
+
         module=new Ln();
         from=0;
-        to=4;
-        step=0.5;
-        accuracy=0.0001;
+        to=8;
+        step=0.1;
+        accuracy=0.001;
 
         CSVLog.logModuleToCSV(module,from,to,step,accuracy);
     }
